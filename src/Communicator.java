aa3efc401fa565658ca09a69a63437545a6d44c8
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-10-24 15:22:29 +0200

    Added framework code

---
+++ src/Communicator.java
@@ -0,0 +1,17 @@
+public class Communicator {
+    public void hostNetwork(int port) {
+    }
+
+    public void joinNetwork(String ip, int port) {
+    }
+
+    public void broadcastMove(Move move) {
+    }
+
+    public Move receiveMove(Player playerInTurn) {
+        return null;
+    }
+
+    public void broadcastKey(Card card) {
+    }
+}
