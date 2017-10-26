dde5c93045b5be603c9b8295deb218c089c86444
immerse <thismailisdispensable@gmail.com>
2017-10-26 12:01:18 +0200

    Implemented the deck shuffling protocol and started work on the crypto scheme.

--- src/Deck.java
+++ src/Deck.java
@@ -2,4 +2,26 @@ public class Deck {
     public Card getCard(int index){
         return null;
     }
+
+    public static Deck generatePlainDeck() {
+        return null; // TODO
+
+    }
+
+    public void encryptWithSingleKey(CryptoKey k_i) {
+        // TODO
+    }
+
+    public void shuffle() {
+        // TODO
+        // NOTE: this must be cryptographically secure!!!
+    }
+
+    public void encryptWithMultipleKeys() {
+        // TODO: encrypt every card with its own key.
+    }
+
+    public void decrypt(CryptoKey k_i) {
+        // TODO: decrypt every card with k_i
+    }
 }
