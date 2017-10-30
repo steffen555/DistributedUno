74854641f168adc28e76564419a5e7db655f3af9
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-10-30 15:41:00 +0100

    Reorganized UnoGame class to prepare for refactor.

--- src/UnoGame.java
+++ src/UnoGame.java
@@ -7,20 +7,107 @@ public class UnoGame {
     private Communicator comm;
     private PlayerGroup players;
     private Pile pile;
+    private Player currentPlayer;
 
     public UnoGame(Communicator comm) {
         this.comm = comm;
-
         pile = new Pile();
         players = new PlayerGroup(comm.getPlayers());
+        currentPlayer = getCurrentPlayer();
+        shuffleDeck();
+        distributeHands();
+    }
 
+    private Player getCurrentPlayer() {
+        return players.getPlayerInTurn();
+    }
+
+    private void shuffleDeck(){
         DeckShufflingProtocol deckCreator = new DeckShufflingProtocol(comm, players);
         deck = deckCreator.makeShuffledDeck();
+    }
 
+    private void distributeHands(){
         // now distribute keys and so on to initialize each player's hand,
         // as well as the pile.
-        HandDistributionProtocol distributer = new HandDistributionProtocol(comm, deck, pile, players);
-        distributer.distributeInitialCards();
+        HandDistributionProtocol distributor = new HandDistributionProtocol(comm, deck, pile, players);
+        distributor.distributeInitialCards();
+    }
+
+    private Player getNextPlayer() {
+        throw new NotImplementedException();
+    }
+
+    private Move getMoveFromCurrentPlayer() {
+        return getMoveFromPlayer(currentPlayer);
+    }
+
+    private boolean isLegal(Move move) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Does the concrete action of performing the move. The move given is checked to be valid.
+     * The move can be either playing a card to the pile or drawing a new card.
+     * If a card is drawn, the user should have the option to play any card.
+     */
+    private void doMove(Move move) {
+        if (move.getType() == MoveType.PLAY) {
+            doPlayMove(move);
+        } else if (move.getType() == MoveType.DRAW) {
+            doDrawMove(move);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Performs the action of drawing a card from the deck.
+     */
+    private void doDrawMove(Move move) {
+        // since the top card in the deck is going to playerInTurn's hand,
+        // that means that every player except him must share the key
+        // with every other player.
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    private boolean currentPlayerHasDrawnThisTurn() {
+        throw new NotImplementedException();
+    }
+
+    private boolean gameOver() {
+        throw new NotImplementedException();
+    }
+
+    private boolean isWinner(Player player) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Performs the action of playing a card to the pile.
+     */
+    private void doPlayMove(Move move) {
+        if (players.myTurn()) {
+            comm.broadcastObject(move.getCard());
+        } else {
+            // receiveKey();
+            // decrypt played card
+            // check if it matches
+            // send OK
+        }
+        throw new NotImplementedException();
+    }
+
+    private Move getMoveFromPlayer(Player player) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns whether a player has won the game
+     */
+    private boolean checkForWinner() {
+        throw new NotImplementedException();
     }
 
     public void run() {
@@ -30,13 +117,6 @@ public class UnoGame {
             players.signalNextTurn();
         } while (!checkForWinner());
         announceWinner();
-    }
-
-    /**
-     * Returns whether a player has won the game
-     */
-    private boolean checkForWinner() {
-        return false; // TODO implement me
     }
 
     private void renderState() {
@@ -58,68 +138,11 @@ public class UnoGame {
         if (players.myTurn())
             comm.broadcastObject(move);
 
-        if (!isLegalMove(move)) {
-            throw new NotImplementedException(); //TODO: Better, please
+        if (!isLegal(move)) {
+            throw new NotImplementedException();
         }
 
         doMove(move);
-    }
-
-    private boolean isLegalMove(Move move) {
-        // TODO implement this
-        return true;
-    }
-
-    /**
-     * Does the concrete action of performing the move. The move given is checked to be valid.
-     * The move can be either playing a card to the pile or drawing a new card.
-     * If a card is drawn, the user should have the option to play any card.
-     */
-    private void doMove(Move move) {
-        if (move.getType() == MoveType.PLAY) {
-            doPlayMove(move);
-        } else if (move.getType() == MoveType.DRAW) {
-            doDrawMove(move);
-        } else {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("Something bad happened");
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /**
-     * Performs the action of playing a card to the pile.
-     */
-    private void doPlayMove(Move move) {
-        if (players.myTurn()) {
-            comm.broadcastObject(move.getCard());
-        } else {
-            // receiveKey();
-            // decrypt played card
-            // check if it matches
-            // send OK
-        }
-        updatePile(null); //TODO: get the card corresponding to the ID from the move
-        updateHand();
-    }
-
-    private void updatePile(Card card) {
-        return; // TODO: implement
-    }
-
-    private void updateHand() {
-        return; // TODO: implement
-    }
-
-    /**
-     * Performs the action of drawing a card from the deck.
-     */
-    private void doDrawMove(Move move) {
-        DrawProtocol drawProtocol = new DrawProtocol(comm, players, deck);
-        drawProtocol.drawCardForCurrentPlayer();
     }
 
     /**
@@ -128,9 +151,4 @@ public class UnoGame {
     private void announceWinner() {
         throw new NotImplementedException();
     }
-
-    private Move promptPlayerForMove() {
-        throw new NotImplementedException();
-    }
-
 }
