cd4ac4e88943156cc92d3ef2c0908f8ead2e490c
immerse <thismailisdispensable@gmail.com>
2017-12-04 16:44:50 +0100

    added python scripts to carry out experiments

---
+++ experiments/number_of_messages.py
@@ -0,0 +1,29 @@
+import spawnlib
+import os
+
+os.system("rm *log.txt* 2>/dev/null")
+
+spawnlib.run_with_n_players(20)
+
+num_broadcast = []
+num_sent = []
+
+def process_logfile(filename):
+	for line in open(filename):
+		if "Number of messages sent:" in line:
+			num = int(line.split(": ")[2])
+			num_sent.append(num)
+			
+		if "Number of messages broadcast:" in line:
+			num = int(line.split(": ")[2])
+			num_broadcast.append(num)
+
+
+
+for filename in os.listdir("."):
+	if filename.startswith("log.txt"):
+		process_logfile(filename)
+
+
+print "broadcast:", num_broadcast
+print "sent:     ", num_sent
---
+++ experiments/spawnlib.py
@@ -0,0 +1,90 @@
+from subprocess import Popen, PIPE
+import time
+import fcntl, os
+
+PORT_BASE = 5000
+
+def set_nonblocking(fd):
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    flags = flags | os.O_NONBLOCK
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+
+def spawn(i, num_players=2):
+	my_port = PORT_BASE + i
+
+	if i == 0:
+		# this is the host
+		cmd = "java Main %d %d" % (my_port, num_players)
+	else:
+		# join the host
+		host_port = PORT_BASE
+		cmd = "java Main %d 127.0.0.1 %d" % (my_port, host_port)
+	
+	return Popen(cmd, shell=True, stdin = PIPE, stdout = PIPE, stderr = PIPE, bufsize = 1)
+
+class Process:
+	def __init__(self, i, num_players=2):
+		self.p = spawn(i, num_players)
+
+		set_nonblocking(self.p.stdout)
+		set_nonblocking(self.p.stderr)
+
+		self.read_until("your name")
+		self.write("peer #%d\n" % i)
+
+		if i == 0:
+			self.read_until("hosting")
+		else:
+			self.read_until("joining")
+
+		time.sleep(1)
+
+	def read(self):
+		time.sleep(0.2)
+		result = ""
+		while True:
+			try:
+				result += self.p.stdout.read(1)
+			except IOError:
+				break
+		
+		return result
+	
+	def read_until(self, text):
+		result = ""
+		while True:
+			if text in result:
+				break
+			try:
+				result += self.p.stdout.read(1)
+			except IOError:
+				print result
+				time.sleep(0.1)
+
+		return result
+
+	
+	def write(self, text):
+		return self.p.stdin.write(text)
+
+
+def run_with_n_players(num_players):
+	os.system("killall -KILL java 2>/dev/null") # clean up old experiments..
+
+	players = [Process(i, num_players=num_players) for i in range(num_players)]
+
+	# let them all autoplay
+	for p in players:
+		p.read_until("Pile")
+		p.write("a\n")
+		p.read_until("Pile")
+
+	# let them finish
+	done = False
+	while not done:
+		for p in players:
+			result = p.read()
+			print result
+			if "is the winner" in result:
+				done = True
+
