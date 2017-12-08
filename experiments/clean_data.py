2f8eb700ea014a4503a17b24f35eb76c5715c578
immerse <thismailisdispensable@gmail.com>
2017-12-08 11:30:09 +0100

    started cleaning up data

---
+++ experiments/clean_data.py
@@ -0,0 +1,65 @@
+import os
+
+outdir = "cleaned_data/"
+
+def average(ns):
+	return float(sum(ns))/len(ns)
+
+
+def handle_tpa(filename):
+	f1 = open(outdir + "tpa_shuffle_times.txt", "a")
+	f2 = open(outdir + "tpa_draw_times.txt", "a")
+	f3 = open(outdir + "tpa_play_times.txt", "a")
+
+	for line in open(filename):
+
+		num_players = int(line.split(" ")[0])
+
+		times = eval("[" + line[line.find(" [")+2:])
+
+		if len(times) == 0:
+			continue # no data..
+
+		point = average(times)
+
+		if "Shuffle times:" in line:
+			f1.write("%s,%s\n" % (num_players, point))
+
+		elif "Draw move times:" in line:
+			f2.write("%s,%s\n" % (num_players, point))
+
+		elif "Play move times:" in line:
+			f3.write("%s,%s\n" % (num_players, point))
+
+		else:
+			print "weird line:", line
+			
+
+
+def handle_bandwidth(filename):
+	pass
+
+def handle_num_messages(filename):
+	pass
+
+def handle_crypto(filename):
+	pass
+
+
+os.system("rm cleaned_data/*")
+
+for filename in os.listdir("real_data"):
+	filename = "real_data/" + filename
+
+	if "bandwidth" in filename:
+		handle_bandwidth(filename)
+	
+	if "number_of_messages_experiment" in filename:
+		handle_num_messages(filename)
+	
+	if "crypto_experiment" in filename:
+		handle_crypto(filename)
+	
+	if "time_per_action" in filename:
+		handle_tpa(filename)
+
